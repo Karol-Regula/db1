1e8d2097cfec4aafeb4088bfc31365776ec05f08
Karol-Regula <kregula@stuy.edu>
2016-10-14 12:27:45 -0400

    Edited the dictionary printing in csv_tester.py to print only the values in fields.

--- csv_tester.py
+++ csv_tester.py
@@ -3,11 +3,22 @@ import csv
 fObj = open("peeps.csv") 
 d=csv.DictReader(fObj)
 
+print "Printing entire dictionary:"
 for k in d:
     print k
-    #Q: What can you print here to make each line show only
-    #   a name and its ID?
-    #   eg,
-    #   sasha, 3
+
+print '\n'
+
+fObj = open("peeps.csv") 
+d=csv.DictReader(fObj)
+
+print "Printing formatted dictionary:"
+for k in d:
+    print k['age'] + ", " + k['name'] + ", " + k['id']
+
 
     
+#Q: What can you print here to make each line show only
+#   a name and its ID?
+#   eg,
+#   sasha, 3
