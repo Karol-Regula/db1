0aca8d95305c4adeb52b39d29cb1ef0a9131d6ff
Karol-Regula <kregula@stuy.edu>
2016-10-14 12:29:30 -0400

    Added printing of values in courses.csv.

--- csv_tester.py
+++ csv_tester.py
@@ -8,13 +8,20 @@ for k in d:
     print k
 
 print '\n'
-
 fObj = open("peeps.csv") 
 d=csv.DictReader(fObj)
 
 print "Printing formatted dictionary:"
 for k in d:
     print k['age'] + ", " + k['name'] + ", " + k['id']
+    
+print '\n'
+fObj = open("courses.csv") 
+d=csv.DictReader(fObj)
+
+print "Printing formatted dictionary:"
+for k in d:
+    print k['code'] + ", " + k['mark'] + ", " + k['id']
 
 
     
