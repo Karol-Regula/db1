60508b4ac0c52e6792534ceaf0c33ac769506fae
Karol-Regula <kregula@stuy.edu>
2016-10-16 14:20:17 -0400

    Created table with students.

--- db_builder.py
+++ db_builder.py
@@ -1,21 +1,29 @@
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
 
-
 f="discobandit.db"
-
 db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 
 #==========================================================
 #INSERT YOUR POPULATE CODE IN THIS ZONE
-#...perhaps by beginning with these examples...
+
+students = open("peeps.csv") #open csv file
+d=csv.DictReader(students) #read csv file into dictionary
+
+c.execute("CREATE TABLE students (name TEXT, id INTEGER, age INTEGER);") #create table
+for k in d:
+    c.execute("INSERT INTO students VALUES('" + k['name'] + "'," + k['id'] + "," + k['age'] + ")") #add data to table
+    
+for row in c.execute('SELECT * FROM students'):
+        print row #print data from table
+        #data printed with 'u' in front, however when the table is inpected in sqlite3 it is fine
+# print (c.execute("SELECT * FROM students;")) #this seems to be useless
+#c.execute(q)    #run SQL query #I did this separately for each line
+
+
 
 '''
-q = "CREATE TABLE students (name TEXT, id INTEGER)"
-
-c.execute(q)    #run SQL query
-
 
 q = "CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER)"
 
