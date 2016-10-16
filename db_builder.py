e8eb476b3c8fb8607b856855793f8886b338d070
oreo21 <reok99@gmail.com>
2016-10-16 18:16:44 -0400

    updated to generate 'courses' table

--- db_builder.py
+++ db_builder.py
@@ -10,18 +10,18 @@ c = db.cursor()    #facilitate db ops
 #INSERT YOUR POPULATE CODE IN THIS ZONE
 
 students = open("peeps.csv") #open csv file
-d=csv.DictReader(students) #read csv file into dictionary
+d1 = csv.DictReader(students) #read csv file into dictionary
 
 c.execute("CREATE TABLE students (id INTEGER, name TEXT, age INTEGER);") #create table
-for k in d:
+for k in d1:
     c.execute("INSERT INTO students VALUES(" + k['id'] + ",'" + k['name'] + "'," + k['age'] + ");") #add data to table
 
-'''
+courses = open("courses.csv")
+d2 = csv.DictReader(courses)
 
-q = "CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER)"
-
-c.execute(q)
-'''
+c.execute("CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER);")
+for k in d2:
+	c.execute("INSERT INTO courses VALUES('" + k['code'] + "'," + k['mark'] + "," + k['id'] + ");")
 
 #==========================================================
 db.commit() #save changes
