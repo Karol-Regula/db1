518819e549bb35d845210f98f2248966a5095d81
Karol-Regula <kregula@stuy.edu>
2016-10-19 13:45:22 -0400

    Got values from database.

--- get_averages.py
+++ get_averages.py
@@ -6,10 +6,14 @@ db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 
 def printer():
-    get = "SELECT name
+    get = "SELECT name, mark FROM students, courses WHERE students.id = courses.id;"
+    out = (c.execute(get))
+    print out;
+    for i in out:
+        print i[0]
+        print i[1]
 
 
-
-
+printer()
 db.commit() #save changes
 db.close()  #close database
