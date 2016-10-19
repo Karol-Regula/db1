44ec3a294ad09ef916b73ed4bd4dc78f15d6d9fb
oreo21 <reok99@gmail.com>
2016-10-19 17:27:37 -0400

    get_averages.py finished

--- get_averages.py
+++ get_averages.py
@@ -1,19 +1,38 @@
+
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
 
-f="discobandit.db"
+
+f ="discobandit.db"
 db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 
-def printer():
-    get = "SELECT name, mark FROM students, courses WHERE students.id = courses.id;"
-    out = (c.execute(get))
-    print out;
-    for i in out:
-        print i[0]
-        print i[1]
+
+#pre: name must exist 
+def computeAvg(name):
+	get = "SELECT name, mark FROM students, courses WHERE students.id = courses.id;"
+	data = (c.execute(get))
+	total = 0
+	tally = 0
+	for record in data:
+		if record[0] == name:
+			total += record[1]
+			tally += 1
+	avg = total / tally
+	return avg
 
 
-printer()
+def displayAverages():
+	get = "SELECT name, mark, students.id FROM students, courses WHERE students.id = courses.id;"
+	data = (c.execute(get))
+	name = ""
+	for record in data:
+		if not (record[0] == name): #if name hasn't come up before
+			print '%s (id %d) has an average of %d'%(record[0], record[2], record[1])
+			name = record[0]
+
+
+displayAverages()
+#print computeAvg('kruder')
 db.commit() #save changes
 db.close()  #close database
